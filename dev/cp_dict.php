3c73a976bcc651d3bf1da060a676429841a88693
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-03-14 16:58:33 +0900

    Delete.

--- dev/cp_dict.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = 'php_dict.txt';
-$f = get_defined_functions();
-sort( $f['internal'] );
-$dict = implode( "\n", $f['internal'] );
-file_put_contents($file, $dict);
-echo 'Exported, The php method to a Text File.' . "\n";
-?>
